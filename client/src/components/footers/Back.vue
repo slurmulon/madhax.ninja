5e750b10cc7d4ee001610a0641ab744f23306a06
evavro <me@madhax.io>
2018-07-04 23:17:25 -0700

    :tv: :checkered_flag: functional back button in footer

--- client/src/components/footers/Back.vue
+++ client/src/components/footers/Back.vue
@@ -1,10 +1,9 @@
 <template>
   <v-layout column nowrap>
     <v-flex xs1>
-      <v-btn icon fab light small color="secondary" class="elevation-12">
+      <v-btn icon fab light small color="secondary" @click="$router.push('/')">
         <v-icon light>chevron_left</v-icon>
       </v-btn>
     </v-flex>
   </v-layout>
 </template>
-
