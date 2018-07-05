22aa044aa20758be52a3fc2cab80b0b078df4271
evavro <me@madhax.io>
2018-07-04 19:37:23 -0700

    :tv: :lipstick: :bug: :checkered_flag: working checkpoint for style repairs

---
+++ client/src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <!-- <nav class="header"> -->
+      <slot name="header"></slot>
+    <!-- </nav> -->
+    <main>
+      <slot></slot>
+    </main>
+    <!-- <footer> -->
+      <slot name="footer"></slot>
+    <!-- </footer> -->
+  </v-app>
+</template>
+
+<style lang="stylus">
+@require '../styles/main'
+
+#app, .application--wrap
+  display flex
+  flex 1
+  flex-flow column wrap
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Erik Vavro</title>
-    <link href="https://fonts.googleapis.com/css?family=Geo|Varela+Round|Yesteryear" rel="stylesheet">
-    <link rel="stylesheet" href="./styles/main.css">
-  </head>
-  <body>
-    <nav class="header">
+<template>
+  <layout>
+    <nav slot="header" class="header">
+    <!-- <div slot="header"> -->
       <div class="logo">
         <h1>Erik Vavro</h1>
         <span>Software (engineer + architect)</span>
@@ -18,13 +12,15 @@
         <!-- <a href="#">Blog</a> -->
         <a href="mailto:evavro@gmail.com">Contact</a>
       </div>
+    <!-- </div> -->
     </nav>
-    <main>
-      <section class="content">
-        <strong class="shout">Progressive Web Applications, RESTful APIs, Hypermedia + More</strong>
-      </section>
-    </main>
-    <footer>
+
+    <section class="content">
+      <strong class="shout">Progressive Web Applications, RESTful APIs, Hypermedia + More</strong>
+    </section>
+
+    <footer slot="footer">
+    <!-- <div slot="footer"> -->
       <section class="status">
         <h2 class="head">Status</h2>
         <div class="entry">
@@ -42,29 +38,39 @@
       </section>
       <section class="tools">
         <h2 class="head">Tools</h2>
-        <div class="card">
-          <a href="https://nodejs.org/" target="tool"><img src="./assets/tools/node-js.png"></a>
-        </div>
-        <div class="card">
-          <a href="https://clojure.org/" target="tool"><img src="./assets/tools/clojure.png"></a>
-        </div>
-        <div class="card">
-          <a href="https://elixir-lang.org/" target="tool"><img src="./assets/tools/elixir.png"></a>
-        </div>
-        <div class="card">
-          <a href="https://vuejs.org/" target="tool"><img src="./assets/tools/vuejs.png"></a>
-        </div>
-        <div class="card">
-          <a href="http://json-schema.org/" target="tool"><img src="./assets/tools/json-schema.png"></a>
-        </div>
-        <div class="card">
-          <a href="https://webpack.js.org/" target="tool"><img src="./assets/tools/webpack.png"></a>
-        </div>
-        <div class="card">
-          <a href="https://apiblueprint.org/" target="tool"><img src="./assets/tools/api-blueprint.png"></a>
-        </div>
+        <!-- <div class="card"> -->
+        <!--   <a href="https://nodejs.org/" target="tool"><img src="./assets/tools/node-js.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="https://clojure.org/" target="tool"><img src="./assets/tools/clojure.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="https://elixir-lang.org/" target="tool"><img src="./assets/tools/elixir.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="https://vuejs.org/" target="tool"><img src="./assets/tools/vuejs.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="http://json-schema.org/" target="tool"><img src="./assets/tools/json-schema.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="https://webpack.js.org/" target="tool"><img src="./assets/tools/webpack.png"></a> -->
+        <!-- </div> -->
+        <!-- <div class="card"> -->
+        <!--   <a href="https://apiblueprint.org/" target="tool"><img src="./assets/tools/api-blueprint.png"></a> -->
+        <!-- </div> -->
       </section>
+    <!-- </div> -->
     </footer>
-  </body>
-</html>
+  </layout>
+</template>
 
+<script>
+import Layout from '@/components/Layout'
+
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
