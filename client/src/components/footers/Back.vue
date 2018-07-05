88534e4df4b0a1eab8b49ee8d42b2745882ddccf
evavro <me@madhax.io>
2018-07-04 23:18:41 -0700

    :tv: :wrench: :sparkles: use go -1 instead of push for back button

--- client/src/components/footers/Back.vue
+++ client/src/components/footers/Back.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout column nowrap>
     <v-flex xs1>
-      <v-btn icon fab light small color="secondary" @click="$router.push('/')">
+      <v-btn icon fab light small color="secondary" @click="$router.go(-1)">
         <v-icon light>chevron_left</v-icon>
       </v-btn>
     </v-flex>
