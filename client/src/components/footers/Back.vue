b3e2d72b5343dbd60a71db1b85d59d26099d05b7
evavro <me@madhax.io>
2018-07-04 21:32:31 -0700

    :tv: :pill: playing with cards in Work.vue

--- client/src/components/footers/Back.vue
+++ client/src/components/footers/Back.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout column nowrap>
     <v-flex xs1>
-      <v-btn icon>
-        <v-icon color="primary">arrow_back_ios</v-icon>
+      <v-btn icon fab dark small color="primary">
+        <v-icon dark>chevron_left</v-icon>
       </v-btn>
     </v-flex>
   </v-layout>
--- client/src/views/Work.vue
+++ client/src/views/Work.vue
@@ -3,7 +3,32 @@
     <navbar slot="header"></navbar>
 
     <section class="content">
-      :D
+      <v-card>
+        <v-card-media
+          src="https://s3.amazonaws.com/vuetify-docs/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-card-media>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Top western road trips</div>
+            <span class="grey--text">1,000 miles of wonder</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
     </section>
 
     <footer slot="footer">
