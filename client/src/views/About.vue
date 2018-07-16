668af8be1863ccb6466249ef13b06fe01810ad4a
evavro <me@madhax.io>
2018-07-16 00:57:25 -0700

    :tv: :scroll: :sparkles: grammar improvement

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -22,7 +22,7 @@
                       <p>Hi there! Thanks for giving my site a visit.</p>
                       <p>I am a remote software engineer specializing in progressive web applications with over 15 years of coding experience.</p>
                       <p>I am passionate about learning all things web, creating beautiful apps and writing high-quality code that's built to last.</p>
-                      <p>I believe in life-long learning, focusing on quality right from the get-go and taking a people-first approach to development.</p>
+                      <p>I believe in life-long learning, focusing on quality right from the beginning and taking a people-first approach to development.</p>
                       <span>If you are interested in collaboration or have an opportunity, please feel free to <router-link to="/contact" class="black--text underline">reach out</router-link>.</span>
                       <span>Also be sure to see my <router-link to="/work" class="black--text underline">past and current work</router-link>.</span>
                     </v-flex>
