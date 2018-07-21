b859b82b2b7319d6f4b4e79bf7b412ada01989f2
evavro <me@madhax.io>
2018-07-21 00:18:25 -0700

    :tv: :wrench: integrated aos config

--- client/src/app.js
+++ client/src/app.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './components/App'
 import router from './router'
 import store from './store'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import './setup'
 
 const app = new Vue({
+  created () {
+    AOS.init()
+  },
   router,
   store,
   ...App
