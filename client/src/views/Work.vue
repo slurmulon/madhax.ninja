74962070fb9159134f3a8c0f7721f3e22f1eed85
evavro <me@madhax.io>
2018-08-16 18:40:02 -0700

    :tv: :sparkles: cleanup in Work component

--- client/src/views/Work.vue
+++ client/src/views/Work.vue
@@ -1,111 +1,99 @@
 <template>
-  <!-- <v-content> -->
-    <v-container grid-list-xl fluid>
-      <v-layout row wrap id="work-container">
-        <v-flex xs12 md6 offset-md3 pb-0> <!-- sm3 -->
-          <h1 class="light-blue--text text--lighten-4">Career</h1>
-          <!-- <h1 class="grey--text text--lighten-2">Employment</h1> -->
-          <!-- <h1 class="primary--text">Employment</h1> -->
-        </v-flex>
-        <v-flex xs12 md6 offset-md3 v-for="job in jobs" :key="job.company" class="mb-4"> <!-- sm3 -->
-          <!-- TODO: employmemt, projects, approach, process, philosophy -->
-          <v-card class="subtle-bg elevation-4">
-            <v-card-media
-              :src="require(`@/assets/img/${job.banner}`)"
-              height="200px"
-            ></v-card-media>
+  <v-container grid-list-xl fluid>
+    <v-layout row wrap id="work-container">
+      <v-flex xs12 md6 offset-md3 pb-0> <!-- sm3 -->
+        <h1 class="light-blue--text text--lighten-4">Career</h1>
+        <!-- <h1 class="grey--text text--lighten-2">Employment</h1> -->
+        <!-- <h1 class="primary--text">Employment</h1> -->
+      </v-flex>
+      <v-flex xs12 md6 offset-md3 v-for="job in jobs" :key="job.company" class="mb-4"> <!-- sm3 -->
+        <!-- TODO: employmemt, projects, approach, process, philosophy -->
+        <v-card class="subtle-bg elevation-4">
+          <v-card-media
+            :src="require(`@/assets/img/${job.banner}`)"
+            height="200px"
+          ></v-card-media>
 
-            <v-card-title primary-title>
-              <v-layout column nowrap>
-                <v-flex class="text-xs-center pb-3">
-                  <a :href="job.url">
-                    <img :src="require(`@/assets/img/${job.logo}`)" style="width: 150px">
-                  </a>
-                </v-flex>
-                <v-flex xs1 class="text-xs-center pt-0">
-                  <!-- <div class="headline blocky-flat">{{ job.company }}</div> -->
-                  <div class="blue-grey--text subheading">{{ job.title }}<br/><span class="grey--text body-2 font-weight-thin">{{ job.time }}</span></div>
-                </v-flex>
-                <v-flex xs1 pt-2 pl-4 pr-4>
-                  <!-- {{ job.desc }} -->
-                  <p v-for="detail in job.details" :key="detail" class="font-weight-thin">{{ detail }}</p>
-                </v-flex>
-                <v-flex xs1 pt-2>
-                  <v-layout row wrap justify-end>
-                    <v-flex
-                      xs2
-                      v-for="tool in job.tools"
-                      :key="tool.id"
-                      class="text-xs-center"
-                    >
-                      <!-- {{ tool.icon }} -->
-                      <a :href="tool.url" target="tool">
-                        <img :src="require(`@/assets/img/${tool.icon}`)" class="shiny-icon" style="width: 40px">
-                      </a>
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-              </v-layout>
-            </v-card-title>
-          </v-card>
-        </v-flex>
+          <v-card-title primary-title>
+            <v-layout column nowrap>
+              <v-flex class="text-xs-center pb-3">
+                <a :href="job.url">
+                  <img :src="require(`@/assets/img/${job.logo}`)" style="width: 150px">
+                </a>
+              </v-flex>
+              <v-flex xs1 class="text-xs-center pt-0">
+                <!-- <div class="headline blocky-flat">{{ job.company }}</div> -->
+                <div class="blue-grey--text subheading">{{ job.title }}<br/><span class="grey--text body-2 font-weight-thin">{{ job.time }}</span></div>
+              </v-flex>
+              <v-flex xs1 pt-2 pl-4 pr-4>
+                <!-- {{ job.desc }} -->
+                <p v-for="detail in job.details" :key="detail" class="font-weight-thin">{{ detail }}</p>
+              </v-flex>
+              <v-flex xs1 pt-2>
+                <v-layout row wrap justify-end>
+                  <v-flex
+                    xs2
+                    v-for="tool in job.tools"
+                    :key="tool.id"
+                    class="text-xs-center"
+                  >
+                    <!-- {{ tool.icon }} -->
+                    <a :href="tool.url" target="tool">
+                      <img :src="require(`@/assets/img/${tool.icon}`)" class="shiny-icon" style="width: 40px">
+                    </a>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-card-title>
+        </v-card>
+      </v-flex>
 
-        <v-flex xs12 md6 offset-md3 pt-0 pb-0> <!-- sm3 -->
-          <h1 class="light-blue--text text--lighten-4">Projects</h1>
-          <!-- <h1 class="grey--text text--lighten-2">Projects</h1> -->
-          <!-- <h1 class="primary--text">Projects</h1> -->
-        </v-flex>
-        <v-flex xs12 md6 offset-md3 pt-4>
-          <v-layout row wrap>
-            <v-flex xs12 xl6 v-for="project in projects" :key="project.title" v-if="project.active">
-        <!-- <v-flex xs12 md6 offset-md3 pb-0 v-for="project in projects" :key="project.title"> <1!-- sm3 --1> -->
-          <!-- <v-card class="subtle-bg elevation-4" :href="project.url"> -->
-          <!--   <v-card-title primary-title> -->
-              <!-- <v-layout row nowrap align-center pa-2 style="color: white"> -->
-              <v-layout row nowrap align-start pa-2>
-                <v-flex xs3 class="text-xs-center" pa-0>
-                  <a :href="project.url" target="project">
-                    <img v-if="project.logo" :src="require(`@/assets/img/${project.logo}`)" style="max-width: 65px">
-                    <span v-if="project.emoji" class="display-3 secondary--text cell-shade">{{ project.emoji }}</span>
-                  </a>
-                </v-flex>
-                <v-flex xs9>
-                  <v-layout column wrap>
-                    <v-flex xs1 pt-0 pb-0>
-                      <span class="headline white--text">{{ project.title }}</span>
-                    </v-flex>
-                    <v-flex xs1>
-                      <span class="caption white--text">{{ project.desc }}</span>
-                    </v-flex>
-                    <v-flex xs1>
-                      <v-layout row nowrap justify-end>
-                        <v-flex xs3 v-for="tool in project.tools" :key="tool.id" class="text-xs-right">
-                          <a v-if="tool.icon" :href="tool.url" target="tool" class="cell-shade">
-                            <img :src="require(`@/assets/img/${tool.icon}`)" class="shiny-icon" style="width: 40px">
-                          </a>
-                        </v-flex>
-                      </v-layout>
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-              </v-layout>
-            <!-- </v-card-title> -->
-          <!-- </v-card> -->
-
+      <v-flex xs12 md6 offset-md3 pt-0 pb-0> <!-- sm3 -->
+        <h1 class="light-blue--text text--lighten-4">Projects</h1>
+        <!-- <h1 class="grey--text text--lighten-2">Projects</h1> -->
+        <!-- <h1 class="primary--text">Projects</h1> -->
+      </v-flex>
+      <v-flex xs12 md6 offset-md3 pt-4>
+        <v-layout row wrap>
+          <v-flex xs12 xl6 v-for="project in projects" :key="project.title" v-if="project.active">
+            <v-layout row nowrap align-start pa-2>
+              <v-flex xs3 class="text-xs-center" pa-0>
+                <a :href="project.url" target="project">
+                  <img v-if="project.logo" :src="require(`@/assets/img/${project.logo}`)" style="max-width: 65px">
+                  <span v-if="project.emoji" class="display-3 secondary--text cell-shade">{{ project.emoji }}</span>
+                </a>
+              </v-flex>
+              <v-flex xs9>
+                <v-layout column wrap>
+                  <v-flex xs1 pt-0 pb-0>
+                    <span class="headline white--text">{{ project.title }}</span>
+                  </v-flex>
+                  <v-flex xs1>
+                    <span class="caption white--text">{{ project.desc }}</span>
+                  </v-flex>
+                  <v-flex xs1>
+                    <v-layout row nowrap justify-end>
+                      <v-flex xs3 v-for="tool in project.tools" :key="tool.id" class="text-xs-right">
+                        <a v-if="tool.icon" :href="tool.url" target="tool" class="cell-shade">
+                          <img :src="require(`@/assets/img/${tool.icon}`)" class="shiny-icon" style="width: 40px">
+                        </a>
+                      </v-flex>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
           </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  <!-- </v-content> -->
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import AOS from 'aos'
 import { mapState } from 'vuex'
-
-// TODO: create a centralized bank / store of tools that's shared between About.vue and Work.vue
-// TODO: add relevant links to each job (such as news articles, demos, etc.)
 
 // DOCUMENT: `data` is called before `created` and `mounted`, therefore `methods` and `getters` are not available in this context
 // DOCUMENT: one Vue gotcha is that you always need to provide high-level object keys for data that will be used, even if that data is `null` or `[]` or `{}` initially and is populated asynchronously or in some other defered way. this has to do with how Vue's property data synchronization system works
@@ -121,7 +109,6 @@ export default {
   computed: {
     ...mapState('tools', {
       tools: 'all'
-      // tools: state => state.all
     })
   },
 
@@ -244,11 +231,7 @@ export default {
 
   methods: {
     toolsById (tools) {
-      const res = (this.tools || []).filter(tool => tools.includes(tool.id))
-
-      console.log('tools result', res, this.tools, this)
-
-      return res
+      return (this.tools || []).filter(tool => tools.includes(tool.id))
     }
   }
 }
