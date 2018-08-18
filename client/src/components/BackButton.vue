f9ab670249c364bc7576350986ccacd4827f35c3
evavro <me@madhax.io>
2018-08-17 20:03:55 -0700

    :tv: :lipstick: higher contrast floating button color (black)

--- client/src/components/BackButton.vue
+++ client/src/components/BackButton.vue
@@ -1,5 +1,5 @@
 <template>
   <v-btn fab light small ripple bottom right fixed color="secondary" class="elevation-4" @click="$router.go(-1)">
-    <v-icon light>chevron_left</v-icon>
+    <v-icon light class="black--text">chevron_left</v-icon>
   </v-btn>
 </template>
--- client/src/components/ContactButton.vue
+++ client/src/components/ContactButton.vue
@@ -1,5 +1,5 @@
 <template>
   <v-btn to="/contact" fab light small ripple bottom right fixed color="secondary" class="elevation-4">
-    <v-icon light>chat</v-icon>
+    <v-icon light class="black--text">chat</v-icon>
   </v-btn>
 </template>
