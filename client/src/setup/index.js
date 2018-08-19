d9dbeb464cd9b0b52ca79d97446af44b519dbb73
evavro <me@madhax.io>
2018-08-19 16:26:20 -0700

    :tv: :eyes: :checkered_flag: integrated Google Analytics

--- client/src/setup/index.js
+++ client/src/setup/index.js
@@ -4,6 +4,7 @@ import { sync } from 'vuex-router-sync'
 import router from '../router'
 import store from '../store'
 import theme from './theme'
+import './analytics'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 sync(store, router)
