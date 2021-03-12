1b347a27e091e5af604ccec34b81f411e7f7ae29
Erik Vavro <me@madhax.io>
2021-03-11 23:30:07 -0500

    :tv: :lipstick: :sparkles: Added tooltip to ContactButton

--- client-v2/src/components/ContactButton.vue
+++ client-v2/src/components/ContactButton.vue
@@ -1,21 +1,32 @@
 <template>
-  <v-btn
-    to="/contact"
-    fab
-    light
-    small
-    ripple
-    bottom
-    right
-    fixed
-    color="primary"
-    class="elevation-4"
+  <v-tooltip
+    top
+    open-delay="200"
   >
-    <v-icon
-      light
-      class="black--text"
-    >
-      chat
-    </v-icon>
-  </v-btn>
+    <template #activator="{ on, attrs }">
+      <v-btn
+        to="/contact"
+        fab
+        light
+        small
+        ripple
+        bottom
+        right
+        fixed
+        color="primary"
+        class="elevation-4"
+        v-on="on"
+        v-bind="attrs"
+      >
+        <v-icon
+          light
+          class="black--text"
+        >
+          chat
+        </v-icon>
+      </v-btn>
+    </template>
+
+    <span>Contact me</span>
+  </v-tooltip>
 </template>
