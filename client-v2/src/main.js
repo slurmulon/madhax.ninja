abc225a14d75ce15d1305e3fdf06cf55b70dcebb
Erik Vavro <me@madhax.io>
2021-03-11 19:35:16 -0500

    :package: :wrench: Added and setup Vue composition-api

--- client-v2/src/main.js
+++ client-v2/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+import './plugins/composition'
 import vuetify from './plugins/vuetify'
+import router from './router'
 
 Vue.config.productionTip = false
 
