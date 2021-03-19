d7001e0778a386143fa936a40fcc8717b857268f
Erik Vavro <me@madhax.io>
2021-03-19 16:56:56 -0400

    :tv: :lipstick: :sparkles: Darker book card color in about/Bookshelf

--- client-v2/src/components/about/Bookshelf.vue
+++ client-v2/src/components/about/Bookshelf.vue
@@ -22,7 +22,7 @@
           rounded
           height="100"
           elevation="2"
-          color="rgba(100,100,100,0.1)"
+          color="rgba(22,22,22,0.2)"
           class="d-flex align-center justify-center text-center font-weight-light pa-3"
         >
           {{ book.title }}
