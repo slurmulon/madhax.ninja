915ecfe852c4f364fb6fe2efa958c2c644519109
Erik Vavro <me@madhax.io>
2021-03-19 16:21:16 -0400

    :tv: :scroll: :sparkles: Improved profile text

--- client-v2/src/views/About.vue
+++ client-v2/src/views/About.vue
@@ -10,7 +10,7 @@
         <template #default>
           <v-card-text class="white--text font-weight-light">
             <p>Welcome! My name is Erik Vavro.</p>
-            <p>I'm a software architect specializing in web browser technology with over 18 years of coding experience.</p>
+            <p>I'm a software architect specializing in web and browser technology with over 18 years of coding experience.</p>
             <p>I'm absolutely passionate about learning all things web and creating elegant applications that are built to grow.</p>
             <p>I believe in life-long learning, embracing quality from the beginning and taking a people-first approach to development.</p>
             <span>If you're interested in collaboration or have an opportunity, please feel free to <router-link to="/contact" class="white--text underline">reach out</router-link>.</span>&nbsp;
