09c8bf6100b036d54582ca9ea4ffdb5feeca05d8
Erik Vavro <me@madhax.io>
2021-03-19 20:16:34 -0400

    :tv: :lipstick: :sparkles: Added negative margin-top override to .logo to account for font spacing.

--- client-v2/src/components/Logo.vue
+++ client-v2/src/components/Logo.vue
@@ -3,18 +3,19 @@
     @click="$router.push('/')"
     class="logo display-1 text-decoration-none"
   >
-    <div class="hidden-xs-only">
+    <div class="name hidden-xs-only">
       <span>Erik</span><span class="ml-3">V</span><span style="margin-left: -6px">avro</span>
     </div>
-    <div class="hidden-sm-and-up pr-2">
+    <div class="name hidden-sm-and-up pr-2">
       <span>Erik </span><span class="ml-2">V.</span>
     </div>
   </a>
 </template>
 
 <style lang="sass">
-.logo div
+.logo .name
   font-family: 'Lazer84' !important
   text-shadow: 2px 2px 1px #000
   letter-spacing: 2px //5px
+  margin-top: -5px
 </style>
