f4a4253f7a5602e40dfc350eb422ebe5e1730ddd
Erik Vavro <me@madhax.io>
2021-03-20 01:00:36 -0400

    [v1.0.0] :package: :recycle: :checkered_flag: Re-wrote app using Vuetify 2. Hoisted all client code to the root of repo and removed old app.

--- client/src/components/BackButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-btn fab light small ripple bottom right fixed color="secondary" class="elevation-4" @click="$router.go(-1)">
-    <v-icon light class="black--text">chevron_left</v-icon>
-  </v-btn>
-</template>
