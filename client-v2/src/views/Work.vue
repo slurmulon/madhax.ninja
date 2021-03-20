37b9d0cc3dc8119bb2a8d254fa11c4c44e056c38
Erik Vavro <me@madhax.io>
2021-03-19 21:38:02 -0400

    :tv: :lipstick: :bug: :checkered_flag: Fixed spacing regression in Work view

--- client-v2/src/views/Work.vue
+++ client-v2/src/views/Work.vue
@@ -1,34 +1,41 @@
 <template>
   <page>
-    <section-card title="Career">
-      <template #title-items>
-        <icon-linked-in />
-      </template>
+    <v-row>
+      <!-- TODO: Create SectionCardTitle to remove this duplication -->
+      <v-col
+        cols="12"
+        class="pb-0"
+      >
+        <v-row
+          nowrap
+          align="center"
+        >
+          <v-col class="pb-1">
+            <h1 class="light-blue--text text--lighten-4">Career</h1>
+          </v-col>
+          <v-spacer />
+          <v-col class="text-right">
+            <icon-linked-in />
+          </v-col>
+        </v-row>
+      </v-col>
 
-      <template #default>
-        <v-card-text>
-          <jobs />
-          <projects />
-        </v-card-text>
-      </template>
-    </section-card>
+      <jobs />
+      <projects />
+    </v-row>
   </page>
 </template>
 
 <script>
 import Jobs from '@/components/work/Jobs'
 import Projects from '@/components/work/Projects'
-
 import Page from '@/components/Page'
-import SectionCard from '@/components/SectionCard'
 import IconLinkedIn from '@/components/icons/LinkedIn'
-
 export default {
   components: {
     Jobs,
     Projects,
     Page,
-    SectionCard,
     IconLinkedIn
   }
 }
